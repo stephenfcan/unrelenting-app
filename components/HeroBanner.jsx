372a567644a30677cb44422c5c1ff4cf12aa30d7
Stephen An <stephenan@Stephens-Air.lan>
2022-11-07 03:13:32 -0500

    fix banner styling

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -5,16 +5,19 @@ import { urlFor } from '../lib/client'
 const HeroBanner = ({ heroBanner }) => {
   return (
     <div className='hero-banner-container flex flex-col items-center text-center'>
-      <div>
+      <div className='flex flex-col items-center justify-center'>
         {/* <p className='beats-solo'>{heroBanner.smallText}</p> */}
         <h3>{heroBanner.midText}</h3>
-        {/* <h1 className='z-10 absolute mt-[250px] ml-[435px] text-white font-semibold'>{heroBanner.largeText1}</h1> */}
-        <img src={urlFor(heroBanner.image)} alt="" className='z-0 relative hero-banner-image md:px-10 w-screen md:w-[1060px] md:h-[450px]' />
+        <h1 className='z-10 absolute my-auto mx-auto text-white'>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="" className='z-0 object-cover relative hero-banner-image md:px-10 w-screen md:w-[1060px] md:h-[450px]' />
+        <Link href={`/product/${heroBanner.product}`}>
+          <button className='absolute mt-20 hover:shadow-lg font-semibold w-[200px] px-[5px] py-[13px] bg-black text-white' type='button'>{heroBanner.buttonText}</button>
+        </Link>
 
         <div>
-          <Link href={`/product/${heroBanner.product}`}>
-            <button className='absolute -mt-[125px] md:-mt-[170px] -mx-[125px] hover:shadow-lg font-semibold w-[250px] px-[5px] py-[13px] bg-black text-white' type='button'>{heroBanner.buttonText}</button>
-          </Link>
+          {/* <Link href={`/product/${heroBanner.product}`}>
+            <button className='absolute -mt-[80px] md:-mt-[170px] -mx-[100px] hover:shadow-lg font-semibold w-[200px] px-[5px] py-[13px] bg-black text-white' type='button'>{heroBanner.buttonText}</button>
+          </Link> */}
           {/* <div className='desc'>
             <h5>Description</h5>
             <p>{heroBanner.desc}</p>
