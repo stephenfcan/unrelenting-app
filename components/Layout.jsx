6680596c43fa64fad83f4dba8682e0cd94a57d5e
Stephen An <stephenan@Stephens-Air.lan>
2022-11-14 19:43:31 -0500

    stretch footer background across full screen

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,23 +8,25 @@ import HamburgerMenu from './HamburgerMenu'
 const Layout = ({ children }) => {
   const { showHamburgerMenu } = useStateContext();
   return (
-    <div className='flex flex-col min-h-screen max-w-7xl mx-auto'>
-      <Head>
-        <title>UNRELENTING</title>
-      </Head>
+    <div>
+      <div className='flex flex-col min-h-screen max-w-7xl mx-auto'>
+        <Head>
+          <title>UNRELENTING</title>
+        </Head>
 
-      <header className='sticky top-0 z-50 bg-white'>
-        <NavBar />
-      </header>
+        <header className='sticky top-0 z-50 bg-white'>
+          <NavBar />
+        </header>
 
-      <main className='main-container'>
-        { showHamburgerMenu ? <HamburgerMenu /> : children}
-      </main>
-
+        <main className='main-container'>
+          { showHamburgerMenu ? <HamburgerMenu /> : children}
+        </main>
+      </div>
       <footer className='mt-auto bottom-0 pt-10'>
         <Footer />
       </footer>
     </div>
+    
   )
 }
 
